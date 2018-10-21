d9d8952df7d91821e523bbe6b044da17c8ff6d2b
Alexander Zeier <alexander.zeier@h-da.de>
2018-10-21 14:25:47 +0200

    task files

---
+++ task_generation/generated/dbSchema.sql
@@ -0,0 +1,2 @@
+INSERT INTO "conditions" VALUES(1, 0, 'task_library_[t].ipynb', '7f96cd9dad3de37bde2667a6b8ac1fb6e62b35a72a370f24342e8ecbb1ebd4de');
+INSERT INTO "conditions" VALUES(1, 1, 'task_bc_library_[t].ipynb', 'e69085be8e414b0a48ff2c7b3553cda3b81adc9b8a012ba01635dd37608f14d8');
